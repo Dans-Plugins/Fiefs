4e3f2fe033d0140f86294a92712daeb80877e214
dmccoystephenson <dmccoystephenson@gmail.com>
2021-09-07 12:37:20 -0600

    Added calls to the Storage Manager to the main class

--- src/main/java/dansplugins/fiefs/Fiefs.java
+++ src/main/java/dansplugins/fiefs/Fiefs.java
@@ -1,5 +1,6 @@
 package dansplugins.fiefs;
 
+import dansplugins.fiefs.managers.StorageManager;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -26,11 +27,13 @@ public final class Fiefs extends JavaPlugin {
             }
             return;
         }
+
+        StorageManager.getInstance().load();
     }
 
     @Override
     public void onDisable() {
-
+        StorageManager.getInstance().save();
     }
 
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
