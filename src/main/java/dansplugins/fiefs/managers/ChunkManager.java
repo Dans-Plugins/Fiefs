505a5668904cc438370b17ab12fd3717fc4ac1d7
dmccoystephenson <dmccoystephenson@gmail.com>
2021-09-07 21:59:40 -0600

    Created some skeleton methods in ChunkManager.java

--- src/main/java/dansplugins/fiefs/managers/ChunkManager.java
+++ src/main/java/dansplugins/fiefs/managers/ChunkManager.java
@@ -1,5 +1,36 @@
 package dansplugins.fiefs.managers;
 
+import dansplugins.factionsystem.objects.Faction;
+import org.bukkit.Chunk;
+
 public class ChunkManager {
-    // TODO: implement
+
+    private static ChunkManager instance;
+
+    private ChunkManager() {
+
+    }
+
+    public static ChunkManager getInstance() {
+        if (instance == null) {
+            instance = new ChunkManager();
+        }
+        return instance;
+    }
+
+    public Chunk getClaimedChunk(Chunk chunk) {
+        // TODO: implement
+        return null;
+    }
+
+    public boolean claimChunk(Chunk chunk, Faction faction) {
+        // TODO: implement
+        return false;
+    }
+
+    public boolean unclaimChunk(Chunk chunk, Faction faction) {
+        // TODO: implement
+        return false;
+    }
+
 }
