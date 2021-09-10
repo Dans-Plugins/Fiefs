b162266fb47415708d9ffe6d80a9036b7fd96cb1
dmccoystephenson <dmccoystephenson@gmail.com>
2021-09-09 22:43:52 -0600

    Changed version to v0.5

--- src/main/java/dansplugins/fiefs/Fiefs.java
+++ src/main/java/dansplugins/fiefs/Fiefs.java
@@ -13,7 +13,7 @@ public final class Fiefs extends JavaPlugin {
 
     private static Fiefs instance;
 
-    private final String version = "v0.4";
+    private final String version = "v0.5";
 
     public static Fiefs getInstance() {
         return instance;
