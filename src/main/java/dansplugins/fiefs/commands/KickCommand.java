fc8f22658e2a8b0ea5193c6109d736ac086f9bf5
dmccoystephenson <dmccoystephenson@gmail.com>
2021-09-09 23:01:56 -0600

    Added some skeleton classes for more commands

---
+++ src/main/java/dansplugins/fiefs/commands/KickCommand.java
@@ -0,0 +1,12 @@
+package dansplugins.fiefs.commands;
+
+import org.bukkit.command.CommandSender;
+
+public class KickCommand {
+
+    public boolean execute(CommandSender sender) {
+        // TODO: implement
+        return true;
+    }
+
+}
