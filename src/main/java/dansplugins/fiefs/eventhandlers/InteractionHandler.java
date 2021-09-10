58af76706e208c36fcea6ee79311371167f825b3
dmccoystephenson <dmccoystephenson@gmail.com>
2021-09-10 00:04:42 -0600

    Implemented event handler method for the Block Break event

--- src/main/java/dansplugins/fiefs/eventhandlers/InteractionHandler.java
+++ src/main/java/dansplugins/fiefs/eventhandlers/InteractionHandler.java
@@ -1,6 +1,13 @@
 package dansplugins.fiefs.eventhandlers;
 
+import dansplugins.factionsystem.externalapi.MF_Faction;
+import dansplugins.fiefs.Fiefs;
+import dansplugins.fiefs.MedievalFactionsIntegrator;
+import dansplugins.fiefs.data.PersistentData;
+import dansplugins.fiefs.managers.ChunkManager;
 import dansplugins.fiefs.objects.ClaimedChunk;
+import dansplugins.fiefs.objects.Fief;
+import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -13,7 +20,32 @@ public class InteractionHandler implements Listener {
 
     @EventHandler()
     public void handle(BlockBreakEvent event) {
-        // TODO: implement
+        Player player = event.getPlayer();
+
+        Block brokenBlock = event.getBlock();
+        ClaimedChunk claimedChunk = ChunkManager.getInstance().getClaimedChunk(brokenBlock.getChunk());
+        if (claimedChunk == null) {
+            return;
+        }
+
+        MF_Faction playersFaction = MedievalFactionsIntegrator.getInstance().getAPI().getFaction(player);
+        if (playersFaction == null) {
+            return;
+        }
+
+        if (!playersFaction.getName().equalsIgnoreCase(claimedChunk.getFaction())) {
+            return;
+        }
+
+        Fief playersFief = PersistentData.getInstance().getFief(player);
+        if (playersFief == null) {
+            return;
+        }
+
+        if (!claimedChunk.getFief().equalsIgnoreCase(playersFief.getName())) {
+            if (Fiefs.getInstance().isDebugEnabled()) { System.out.println("[DEBUG] Cancelling Block Break event."); }
+            event.setCancelled(true);
+        }
     }
 
     @EventHandler()
