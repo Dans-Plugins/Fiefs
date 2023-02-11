7587e8f6bd6f10cdcd2e5518ca7da72aa1f7a10c
dmccoystephenson <dmccoystephenson@gmail.com>
2023-02-11 14:20:43 -0700

    Made MF accessible from the Fiefs class.

--- src/main/kotlin/com/dansplugins/fiefs/Fiefs.kt
+++ src/main/kotlin/com/dansplugins/fiefs/Fiefs.kt
@@ -1,10 +1,13 @@
 package com.dansplugins.fiefs
 
+import com.dansplugins.factionsystem.MedievalFactions
 import com.dansplugins.fiefs.command.fiefs.FiefsCommand
 import org.bstats.bukkit.Metrics
 import org.bukkit.plugin.java.JavaPlugin
 
 class Fiefs : JavaPlugin() {
+    lateinit var medievalFactions: MedievalFactions
+
     override fun onEnable() {
         saveDefaultConfig()
         config.options().copyDefaults(true)
@@ -12,6 +15,15 @@ class Fiefs : JavaPlugin() {
         saveConfig()
 
         Metrics(this, 12743)
+
+        val medievalFactions = server.pluginManager.getPlugin("MedievalFactions") as? MedievalFactions
+        if (medievalFactions == null) {
+            logger.severe("A compatible version of Medieval Factions was not found. Are you using Medieval Factions 5?")
+            isEnabled = false
+            return
+        } else {
+            this.medievalFactions = medievalFactions
+        }
 
         getCommand("fiefs")?.setExecutor(FiefsCommand(this))
 
